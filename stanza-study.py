e7e65489502b6b8a479b85a5d531d676c2c5a4e0
Tsubasa Kato <tsubasa@superai.online>
2021-06-26 09:34:30 +0900

    Updated.

--- stanza-study.py
+++ stanza-study.py
@@ -1,1 +1,10 @@
 #Most of code from sample. Edited to test out.
+import stanza
+
+#Uncomment Below if you need to download English models
+#stanza.download('en')
+
+nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt')
+doc = nlp('What is your favorite food?')
+for token in doc.sentences[0].tokens:
+    print(f'token: {token.text}\twords: {", ".join([word.text for word in token.words])}')
